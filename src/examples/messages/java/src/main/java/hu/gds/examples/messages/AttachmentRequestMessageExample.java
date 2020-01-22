497ff624e2bb735bc330ca2b3964d24ab1f659ad
oliver.nagy <oliver.nagy@arh.hu>
2020-01-22 12:46:24 +0100

    Create AttachmentRequestMessageExample

---
+++ src/examples/messages/java/src/main/java/hu/gds/examples/messages/AttachmentRequestMessageExample.java
@@ -0,0 +1,44 @@
+package hu.gds.examples.messages;
+
+import org.msgpack.core.MessageBufferPacker;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessageUnpacker;
+
+import java.io.IOException;
+
+public class AttachmentRequestMessageExample {
+
+    /*
+        [
+            ..., --> 'header fields'
+            "SELECT meta, data, "@to_valid" FROM "events-@attachment" WHERE id = ’ATID201811071434257890’ and ownerid = ’EVNT201811020039071890’ FOR UPDATE WAIT 86400"
+        ]
+     */
+    public static byte[] packMessage() throws IOException {
+        MessageBufferPacker packer = MessagePack.newDefaultBufferPacker();
+
+        //Wrapper array
+        packer.packArrayHeader(11);
+
+        //HEADER
+        Utils.packHeader(packer, DataType.EVENT.getValue());
+
+        //DATA
+        packer.packString("SELECT meta, data, \"@to_valid\" FROM \"events-@attachment\" WHERE id = ’ATID201811071434257890’ and ownerid = ’EVNT201811020039071890’ FOR UPDATE WAIT 86400");
+
+        return packer.toByteArray();
+    }
+
+    public static void unpackMessage(byte[] message) throws IOException {
+        MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(message);
+
+        //Wrapper array
+        unpacker.unpackArrayHeader();
+
+        //HEADER
+        Utils.unpackHeader(unpacker);
+
+        //DATA
+        String request = unpacker.unpackString();
+    }
+}
