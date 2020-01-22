57359c46f46f8c2739ccac94e10395127b37846f
oliver.nagy <oliver.nagy@arh.hu>
2020-01-22 10:29:33 +0100

    Create EventMessageExample

--- src/examples/messages/java/src/main/java/hu/gds/examples/messages/Main.java
+++ src/examples/messages/java/src/main/java/hu/gds/examples/messages/Main.java
@@ -1,13 +1,13 @@
 package hu.gds.examples.messages;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         //connectionMessageExample();
-        connectionAckMessageExample();
+        //connectionAckMessageExample();
+        eventMessageExample();
     }
 
     private static void connectionMessageExample() throws IOException {
@@ -22,5 +22,10 @@ public class Main {
         byte[] message = ConnectionAckMessageExample.packMessage();
         //unpack message
         ConnectionAckMessageExample.unpackMessage(message);
+    }
+
+    public static void eventMessageExample() throws IOException {
+        //pack message
+        byte[] message = EventMessageExample.packMessage();
     }
 }
