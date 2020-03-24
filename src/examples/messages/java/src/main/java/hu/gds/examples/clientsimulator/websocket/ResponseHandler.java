4f5214b85f32d13bf2c3a283065ea4526d90a90c
oliver.nagy <oliver.nagy@arh.hu>
2020-03-24 14:30:51 +0100

    remove src folder (moved to separate repositories)

--- src/examples/messages/java/src/main/java/hu/gds/examples/clientsimulator/websocket/ResponseHandler.java
+++
@@ -1,8 +0,0 @@
-package hu.gds.examples.clientsimulator.websocket;
-
-import java.io.IOException;
-
-public interface ResponseHandler {
-    void handleResponse(byte[] message) throws IOException;
-    void handleResponse(String message);
-}
